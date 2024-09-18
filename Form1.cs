ad66183aff8029ac8016ead6354f86feb877fddf
Pedro Salazar <salazar9_5@hotmail.com>
2024-09-18 08:01:06 -0700

    metodo suma

--- Form1.cs
+++ Form1.cs
@@ -15,11 +15,20 @@ namespace IntroPOO
         public Form1()
         {
             InitializeComponent();
-        }
+        } 
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var num1 = 7;
+            var num2 = 5;
+            int resultado = Suma(num1, num2);
 
+        }
+
+        private int Suma(int a, int b)
+        {
+            int toalsuma = a + b;
+            return toalsuma;
         }
     }
 }
